96d856d9ec330620903b56178cae4a13db740495
HAMMALE14 <HAMMALE14@09-2100-154>
2011-11-22 14:43:47 -0600

    Started work on the main class

--- Sample/src/me/hammale/sample/sample.java
+++ Sample/src/me/hammale/sample/sample.java
@@ -1,5 +1,27 @@
 package me.hammale.sample;
 
-public class sample {
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class sample extends JavaPlugin {
+
+	  Logger log = Logger.getLogger("Minecraft");
+	
+	@Override
+	public void onEnable() {
+
+		PluginDescriptionFile pdfFile = this.getDescription();
+		log.info("Sample Version: " + pdfFile.getVersion() + " Enabled!");
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		
+		
+	}
 
 }
